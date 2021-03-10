73568fce95e6777c7c0ce7eab6f048ee34cd48c0
GL6BT4 <balint9610@gmail.com>
2021-03-10 11:46:46 +0100

    Pushing project

---
+++ desktop/src/com/mygdx/amusementpark/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.amusementpark.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.amusementpark.AmusementPark;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AmusementPark(), config);
+	}
+}
