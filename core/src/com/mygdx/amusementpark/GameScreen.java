a551f57417d2703c63f02637fac436e90c6fe313
GL6BT4 <balint9610@gmail.com>
2021-03-17 10:43:46 +0100

    Tilok létrehozása és képernyőre kirajzolása.

--- core/src/com/mygdx/amusementpark/GameScreen.java
+++ core/src/com/mygdx/amusementpark/GameScreen.java
@@ -1,29 +1,73 @@
 package com.mygdx.amusementpark;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import org.w3c.dom.css.Rect;
+
+import java.awt.*;
 
 public class GameScreen implements Screen
 {
     final AmusementPark game;
     OrthographicCamera camera;
+    private int window_height = 900;
+    private int window_width = 1200;
+    private int x_size=20;
+    private int y_size=20;
+    private int tile_width = window_width/x_size;
+    private int tile_height = window_height/y_size;
+    Texture tile_texture;
+
+    private Array<Array<Rectangle>> tiles = new Array<Array<Rectangle>>(20);
+    //Ebben tárolódnak
+
+    Rectangle tile;
 
     public GameScreen(final AmusementPark game)
     {
         this.game = game;
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1200, 900);
+        camera.setToOrtho(false, window_width, window_height);
+        tile_texture = new Texture(Gdx.files.internal("tile.png"));
+
+        for(int i = 0; i < y_size; i++)
+        {
+            Array<Rectangle> asd = new Array<Rectangle>();
+            tiles.add(asd);
+            for(int j = 0; j < x_size; j++)
+            {
+                tile = new Rectangle();
+                tile.x=i*tile_width;
+                tile.y=j*tile_height;
+                tile.width=tile_width;
+                tile_height=tile_height;
+                tiles.get(i).add(tile);
+            }
+        }
     }
 
     @Override
     public void render(float delta)
     {
         ScreenUtils.clear(0, 0, 0.2f, 1);
+
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
         //draw dolgok
+
+        for(Array<Rectangle> til : tiles)
+        {
+            for(Rectangle t : til)
+            {
+                game.batch.draw(tile_texture,t.x,t.y);
+            }
+        }
+
 
         game.batch.end();
 
